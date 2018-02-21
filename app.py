a3f5fc9e0d543b24f15c794e9bc668f023c73e92
unknown <barankotar@gmail.com>
2018-02-21 14:11:53 +0400

    'og;

--- app.py
+++ app.py
@@ -1,77 +1,63 @@
-import requests
-import datetime
+import logging
+from queue import Queue
+from threading import Thread
+from telegram import Bot
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Filters
 
-class BotHandler:
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+TOKEN = '504179513:AAEJT5PbCxOXwRJOjgHrTAAc9n4fQ54Tl-A'
 
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-    def get_updates(self, offset=None, timeout=30): #функция для получения всех обновлений
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset} #параметры - offset - для сдвига, пометки уже просмотренных обновлений
-        resp = requests.get(self.api_url + method, params) #к строке запроса прибавляется getUpdates
-        result_json = resp.json()['result']
-        return result_json
+def start(bot, update):
+    update.message.reply_text('welcome MESSAGE')
 
-    def send_message(self, chat_id, text): #функция отправки сообщения. На вход - id-чата и текст
-        params = {'chat_id':chat_id, 'text': text} #формируем параметры сообщения - id-чата и само сообщение
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params) #используем метод пост для отправки сообщения
-        return resp
 
-    def get_last_update(self): #функция получения последнего объекта
-        get_result = self.get_updates()
+def help(bot, update):
+    update.message.reply_text('help message')
 
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = None
 
-        return last_update
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
 
-greet_bot = BotHandler('504179513:AAEJT5PbCxOXwRJOjgHrTAAc9n4fQ54Tl-A')
-greetings = ('здравствуй', 'привет', 'здорово', 'хай')
-now = datetime.datetime.now()
 
-def main():
-    new_offset = None
-    today = now.day
-    hour = now.hour
-    
-    while True:
-        greet_bot.get_updates(new_offset)
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"' % (update, error))
 
-        last_update = greet_bot.get_last_update()
+# Write your handlers here
 
-        if isinstance(last_update, list): 
-            last_update_id = last_update[-1]['update_id'] 
-        elif last_update == None: 
-            continue 
-        else: 
-            last_update_id = last_update['update_id']
-            
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and  6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+def setup(webhook_url=None):
+    """If webhook_url is not passed, run with long-polling."""
+    logging.basicConfig(level=logging.WARNING)
+    if webhook_url:
+        bot = Bot(TOKEN)
+        update_queue = Queue()
+        dp = Dispatcher(bot, update_queue)
+    else:
+        updater = Updater(TOKEN)
+        bot = updater.bot
+        dp = updater.dispatcher
+        dp.add_handler(CommandHandler("start", start))
+        dp.add_handler(CommandHandler("help", help))
 
-        if last_chat_text.lower() in greetings and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Доброе день, {}'.format(last_chat_name))
-            today += 1
+        # on noncommand i.e message - echo the message on Telegram
+        dp.add_handler(MessageHandler(Filters.text, echo))
 
-        if last_chat_text.lower() in greetings and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Доброе вечер, {}'.format(last_chat_name))
-            today += 1
+        # log all errors
+        dp.add_error_handler(error)
+    # Add your handlers here
+    if webhook_url:
+        bot.set_webhook(webhook_url=webhook_url)
+        thread = Thread(target=dp.start, name='dispatcher')
+        thread.start()
+        return update_queue, bot
+    else:
+        bot.set_webhook()  # Delete webhook
+        updater.start_polling()
+        updater.idle()
 
-        new_offset = last_update_id + 1
 
-        
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
-
+    setup()
